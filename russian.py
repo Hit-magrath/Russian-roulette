e9be9e9e2c6d9b185d10c2c7d9b78828e44781f0
Hit-magrath <hitmagrath@gmail.com>
2024-12-02 15:02:44 +0200

    going somewhere

--- russian.py
+++ russian.py
@@ -1,5 +1,27 @@
 import random
 print (''' WELCOME TO MY RUSAIN ROULLETE''')
-thingie = [ "","","",""]
-if thingie[0] == "":
-    print(True)
+# *************************making the empty berral*************************
+berral = [ "","","","","",""]
+#**********************************************************
+
+# *****************randomise the bullet placement**********************
+num = random.choice(range(0,6))
+berral[num] = "b"
+#*****************************************************
+# **************************************shooting mechaninc*****************
+alive = True
+other = 0
+while alive:
+    act = input('''what do yo wish to do?
+    you can spin or shoot or both... ''')
+    if act.lower() == 'a':
+        if other >= 6:
+            other = 0
+        if berral[other] == "":
+            print(True)
+            other+=1
+        else :
+            print("BAAM!!")
+            alive = False
+    if act.lower()=="s":
+        other = random.choice(range(0,6))
