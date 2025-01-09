a1f73cd974f6641fbfb5ac3d8a544a1e56c26f77
Hit-magrath <hitmagrath@gmail.com>
2025-01-09 14:31:39 +0200

    2 player alternations

--- russian.py
+++ russian.py
@@ -11,17 +11,22 @@ berral[num] = "b"
 # **************************************shooting mechaninc*****************
 alive = True
 other = 0
+player = 2
 while alive:
-    act = input('''what do yo wish to do?
-    you can spin or shoot or both... ''')
-    if act.lower() == 'a':
-        if other >= 6:
+    act = input(''' what do yo wish to do?
+     you can spin(s) or shoot(f) or both... ''')
+    if player > 2 :
+        player = 1
+    if act.lower() == 'f':
+        if other > 6:
             other = 0
         if berral[other] == "":
-            print(True)
+            print("you survived...switch to player" + " " + f'{player}')
             other+=1
+            player+=1
         else :
             print("BAAM!!")
             alive = False
     if act.lower()=="s":
+        print("play agian")
         other = random.choice(range(0,6))
